e96d2ef79cadf353671dad36c015a08af8bd7d65
cantuariac <caio.cantuaria@gmail.com>
2021-07-08 16:50:36 -0300

    Projeto reiniciado com django 3.2 Projeto vazio

--- apps/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Registro
-from django.contrib.auth import models
-
-admin.site.register(Registro)
-admin.site.register(Profile)
--- apps/api/permissions.py
+++
@@ -1,9 +0,0 @@
-from copy import deepcopy
-
-from rest_framework import permissions
-
-class DjangoModelPermissionWithGET(permissions.DjangoModelPermissions):
-
-    def __init__(self):
-        self.perms_map = deepcopy(self.perms_map)  # from EunChong's answer
-        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
