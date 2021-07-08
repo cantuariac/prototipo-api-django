219941cad1e2b68bd3dbc8875671097b28bfc452
cantuariac <caio.cantuaria@gmail.com>
2021-07-08 18:19:58 -0300

    Ambiente configurado

--- scripts/initdb.py
+++ scripts/initdb.py
@@ -1,4 +1,4 @@
-from apps.api.models import Registro
+# from apps.api.models import Registro
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -6,5 +6,5 @@ User = get_user_model()
 user = User.objects.create_user('caio', password='1234')
 user.save()
 
-r = Registro(usuario=user, descricao='Registro feito por '+user.username)
-r.save()
+# r = Registro(usuario=user, descricao='Registro feito por '+user.username)
+# r.save()
